9349885d352f9fe6512df7692167b3eacba86cf8
evilgreed <jakub.sanestrzik@spot.pl>
2012-12-07 11:40:18 +0100

    fix

--- configControler.cpp
+++ configControler.cpp
@@ -83,14 +83,14 @@ int configControler::miniScreen(string *title, int size, bool opt){
 		while(1){
 			key = NOKEY;
 			Lcd_Cls();
-			Lcd_Printxy(0,0,1,title.c_str()); //tytul
+			Lcd_Printxy(0, 0, 1, title->c_str()); //tytul
 			// wyswietlanie menu
 			for(int i=0; i < size; i++){
 				string str = options[i];
 				if(marked == 0){
-					Lcd_Printxy(0, (i*8)+8, 1, str.c_str());
+					Lcd_Printxy(0, (i*8)+8, 1, str->c_str());
 				}else{
-					Lcd_Printxy(0, (i*8)+8, 0, str.c_str());
+					Lcd_Printxy(0, (i*8)+8, 0, str->c_str());
 				}
 			}
 			while(1){
