23e8c24e0152f056ba44eafa04eb8b48a27cb5c7
evilgreed <jakub.sanestrzik@spot.pl>
2012-12-27 14:28:07 +0100

    jijij

--- configControler.cpp
+++ configControler.cpp
@@ -493,6 +493,7 @@ int configControler::configReload(){
 	string schtxtimeendminr = cf.Value("ok", "schedule.tx.time.end.min");
 	
 	ip = ipr;
+	delete ipr;
 	port = portr;
 	apn = apnr;
 	user = userr;
