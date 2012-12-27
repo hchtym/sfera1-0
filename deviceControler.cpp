7d0d410489b62b7efbb7d0b2315fbc2a31dd1552
evilgreed <jakub.sanestrzik@spot.pl>
2012-12-27 15:57:40 +0100

    koko

--- deviceControler.cpp
+++ deviceControler.cpp
@@ -152,7 +152,7 @@ while(1){
 			stream = strg;
 			cout << strg << endl;
 			cout << track1 << endl;
-			cout << stream.str() << endl;
+			cout << stream << endl;
 			//return 0;
 		break;
 	        //Track 1 is OK
