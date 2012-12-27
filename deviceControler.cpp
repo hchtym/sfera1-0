9c5016d9d2061f811fbc9d98d971d338140fe346
evilgreed <jakub.sanestrzik@spot.pl>
2012-12-27 14:17:28 +0100

    mkmk

--- deviceControler.cpp
+++ deviceControler.cpp
@@ -122,9 +122,9 @@ int deviceControler::chipCardScan(){
 }
 
 int deviceControler::magCardScan(stringstream &stream){
-	char track1[50];
-	char track2[50];
-	char track3[50];
+	char track1[100];
+	char track2[100];
+	char track3[100];
 	int ret;
 	memset(track1, 0, sizeof(track1));
 	memset(track1, 0, sizeof(track2));
