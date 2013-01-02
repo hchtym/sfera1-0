6ed746410ba5afb89fdc173498cf318421662d1e
evilgreed <jakub.sanestrzik@spot.pl>
2013-01-02 14:31:08 +0100

    johiu

--- masterControler.cpp
+++ masterControler.cpp
@@ -24,7 +24,7 @@ int masterControler::test(){
 		cout << "tu doszedlem " << endl;
 		char id = device->magCardScan(true);
 		//str = device->magCardScan(true);
-		cout << device->magCardScan(false);
+		cout << device->magCardScan(false) << endl;
 		cout << "Co miesci sie w id: " << id << endl;
 		compose << id; 
 		//compose << device->magCardScan(false);
