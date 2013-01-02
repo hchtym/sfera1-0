8507f44e11e61ec81f6e738812d6c84f79ccaa58
evilgreed <jakub.sanestrzik@spot.pl>
2013-01-02 13:56:47 +0100

    kmk

--- masterControler.cpp
+++ masterControler.cpp
@@ -22,7 +22,7 @@ int masterControler::test(){
 		
 	while(1){
 		cout << "tu doszedlem " << endl;
-		char *id; 
+		char id; 
 		id = device->magCardScan(true);
 		//str = device->magCardScan(true);
 		//cout << id << endl;
