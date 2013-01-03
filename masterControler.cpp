40e7d3d3cc090870562130591832919b4ec4b1da
evilgreed <jakub.sanestrzik@spot.pl>
2013-01-03 12:40:49 +0100

    i09

--- masterControler.cpp
+++ masterControler.cpp
@@ -122,6 +122,13 @@ int masterControler::selling(){
 	string sn = config->returnSeriall();
 	cout << "pobrałem: " << sn << endl << "przechodze do file save" << endl; 
 	seller = "00033";
+	date = "2013:01:03:12:14:25";
+	cout << sn << endl;
+	cout << seller << endl;
+	cout << payment << endl;
+	cout << point << endl;
+	cout << extra << endl;
+	cout << date << endl;
 	fileSave(sn, seller, str, payment, point, extra, 0, date);
 	
 }
