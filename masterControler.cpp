6b797cf3a1a4b56a615df40a7f8bdedbc2a1e6a3
evilgreed <jakub.sanestrzik@spot.pl>
2013-01-03 16:53:27 +0100

    kkj

--- masterControler.cpp
+++ masterControler.cpp
@@ -11,12 +11,12 @@ masterControler::masterControler(){
 	config = new configControler();
 	device = new deviceControler();
 	ip = config->returnSerwerIp(); 
-	port = config->returnSerwerPort(); 
-	apn = config->returnGprsApn();
-	user = config->returnGprsUser(); 
-	paswd = config->returnGprsPaswd(); 
-	numerser = config->returnSeriall();
-	network = new networkControler(ip, port, apn, user, paswd, numerser);
+	//port = config->returnSerwerPort(); 
+	//apn = config->returnGprsApn();
+	//user = config->returnGprsUser(); 
+	//paswd = config->returnGprsPaswd(); 
+	//numerser = config->returnSeriall();
+	//network = new networkControler(ip, port, apn, user, paswd, numerser);
 	
 	seller.clear();
 	loginFlag = false;
