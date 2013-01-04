bf0b3bd74b379bdee68e8dc62dd2f8df7440fecb
evilgreed <jakub.sanestrzik@spot.pl>
2013-01-04 16:55:21 +0100

    nun

--- masterControler.cpp
+++ masterControler.cpp
@@ -334,7 +334,7 @@ int masterControler::fileSave(string &sn, string &seler, string &client, string 
 	acttime.Minute = 05;
 	acttime.Second = 55;
 	unsigned char factory[20];
-	strncpy( factory, sn.c_str(), sizeof(factory) );
+	strncpy( (char *)factory, sn.c_str(), sizeof(factory) );
 	//for(int i = 0; i < sn.size(); i++)
 	//{
 	//	factory[i]=(unsigned char )sn[i];
