eb9c912d59f1268884aad561921c77cd5f836821
evilgreed <jakub.sanestrzik@spot.pl>
2013-01-22 16:37:33 +0100

    hniu

--- masterControler.cpp
+++ masterControler.cpp
@@ -34,9 +34,9 @@ void masterControler::setShare(){
 	string msg = "true\n";
 	for(int i = 0; i < msg.size(); i++)
 	{
-		share++ = (void *)msg[i];
+		///share++ = (void *)msg[i];
 	}
-	cout << (char *)share << endl;
+//	cout << (char *)share << endl;
 }
 
 void masterControler::menuShop(){
