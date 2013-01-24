c6bf54f7dc57f9f4347fff3f8c858bc679ec0b0e
evilgreed <jakub.sanestrzik@spot.pl>
2013-01-24 08:55:03 +0100

    njvifugvheu

--- main.cpp
+++ main.cpp
@@ -41,17 +41,19 @@ int main(){
 	pid_t pID = fork();
 	if(pID == 0) /* child */
 	{
+
+		int num;
+		char bufer[6];
+		fd = open(FIFO_NAME, O_RDONLY | O_NDELAY);
+		
+		cout << "Jestem w procesie dziecku" << endl;
+		cout << "(dziecko)adres FD: " << &fd << endl;
+		cout << "( dziecko)zawartosc FD" << fd << endl;
+
 		while(1)
 		{
-			int num;
-			char bufer[6];
-			fd = open(FIFO_NAME, O_RDONLY | O_NDELAY);
-			
-			cout << "Jestem w procesie dziecku" << endl;
-			cout << "adres FD: " << &fd << endl;
-			cout << "zawartosc FD" << fd << endl;
 		
-			sleep(5);
+			sleep(20);
 			cout << "skonczylem czekac sprawdzem co jest w fifo." << endl;
 			do
 			{	
@@ -70,8 +72,10 @@ int main(){
 	}
 	else /*parent */
 	{
-		sleep(2);
+		sleep(3);
 		fd = open(FIFO_NAME, O_WRONLY | O_NDELAY);
+		cout << "(rodzic) adres FD: " << &fd << endl;
+		cout << "(rodzic)zawartosc FD: " << fd << endl;
 		while(1)
 		{
 				master->dispMenu();
