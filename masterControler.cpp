00a92a4324aa7582f916a914db7e5bd38b75d923
evilgreed <jakub.sanestrzik@spot.pl>
2013-02-05 09:55:42 +0100

    hdfiuhi

--- masterControler.cpp
+++ masterControler.cpp
@@ -82,7 +82,7 @@ string masterControler::returnBuildDate()
 	Tokenize(buildDate, compose, ":");
 	for (int i = 0; i < 12 ; ++i)
 	{
-		if((strcmp( (char *)convert.c_str(), month[i]) ) == 0)
+		if((convert.compare(month[i])) == 0)
 		{
 			compose[1] = number[i];
 		}
