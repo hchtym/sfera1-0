e0752c18a83282e5ba96d8baabb18e7da59d21cf
evilgreed <jakub.sanestrzik@spot.pl>
2013-02-05 11:53:46 +0100

    hdisufho

--- masterControler.cpp
+++ masterControler.cpp
@@ -411,7 +411,7 @@ int masterControler::dispMenu()
 					loginFlag = false;
 				}
 			}else{
-				device->printTx("00100200086567","00075","2013-02-05 10:46:52","0020000044");
+				device->printTx("00100200086567","00075","2013-02-05 10:46:52","0020000044", "123,45", "30", "0", "Dla Klienta");
 				// tu bedzie przejscie do sklepu in the future :D jeeeeeee
 			}
 			break;
