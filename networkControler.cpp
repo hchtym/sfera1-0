18c17283efa07785c59e1cdbb4e150e4873549e1
evilgreed <jakub.sanestrzik@spot.pl>
2013-02-11 16:29:47 +0100

    hxiuhiu

--- networkControler.cpp
+++ networkControler.cpp
@@ -311,7 +311,7 @@ int networkControler::softUpdate(string data)
 				cout << "wyswietlam smieci !!" << endl;
 				cout << (char *)pCAPData << endl;
 				//newApp << pCAPData;
-				newApp.write(pCAPData, strlen(pCAPData));
+				newApp.write(pCAPData, ((buffer *10) -1);
 
 				cout << "Pobrałem: " << downloaded << " bajtow" << endl;
 			}
