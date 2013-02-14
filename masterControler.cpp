0d2b101864fcb35bd5b6552b356ddb0eee71afce
evilgreed <jakub.sanestrzik@spot.pl>
2013-02-14 11:08:44 +0100

    ofidjbio

--- masterControler.cpp
+++ masterControler.cpp
@@ -178,7 +178,7 @@ string masterControler::computeTrxNumber(string trxDate)
 	string temporary;
 
 	temporary = vect[6];
-	vect[6] = vect[8]
+	vect[6] = vect[8];
 	vect[8] = temporary;
 
 	for (int i = 0; i < vect.size(); i++)
