20dafd5d5f46c00649f74b1f3b091ef676203f75
evilgreed <jakub.sanestrzik@spot.pl>
2013-02-14 11:18:01 +0100

    hduygdu

--- masterControler.cpp
+++ masterControler.cpp
@@ -722,8 +722,9 @@ int masterControler::dispMenu()
 					loginFlag = false;
 				}
 			}else{
-				string dupa = trxTime();
-				computeTrxNumber(dupa);
+				//string dupa = trxTime();
+				//computeTrxNumber(dupa);
+				selling();
 				//device->printTx("00100200086567","00075","2013-02-05 10:46:52","0020000044", "123,45", "30", "0", "Dla Klienta");
 				//softUpdAck();
 				//check point do sprawdzenia z drukarka :D 
