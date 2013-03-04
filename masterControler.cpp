9626c4aba2aaabbd6fadd497f3f32cb63806120d
evilgreed <jakub.sanestrzik@spot.pl>
2013-03-04 13:26:15 +0100

    hdbhguy

--- masterControler.cpp
+++ masterControler.cpp
@@ -874,10 +874,13 @@ int masterControler::menuScr(const string &menuname,vector<string> &vect, int si
 	    while(1){
 			DelayMs(50);
 			ret = Mcr_Read((BYTE *)track1, (BYTE *)track2, (BYTE *)track3);
+			cout << "zawartosc ret: " << ret << endl;
 			if (ret&0x80){
 				if(ret & 1) state = 1;
 				if(ret & 2) state = 2;
 				if(ret & 3) state = 3;
+				cout << "zawartosc ret: " << ret << endl;
+				cout << "zawartosc state: " << state << endl;
 
 				cout << "wykrylem swipe w menu" << endl;
 				if(!loginFlag){
