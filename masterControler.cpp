79c0643b2e6a4f4a0177cb9f76c874b2ccada24d
evilgreed <jakub.sanestrzik@spot.pl>
2013-03-05 11:22:14 +0100

    ndsjkvhb jh

--- masterControler.cpp
+++ masterControler.cpp
@@ -1181,6 +1181,48 @@ int masterControler::menuService()
 			{
 				return 0;
 			}
+			switch(usItem)
+			{
+				case 0:
+					//device->
+				break;
+				case 1:
+					device->rfidRead();
+				break;
+				case 2:
+
+				break;
+				case 3:
+
+				break;
+				case 4:
+
+				break;
+				case 5:
+
+				break;
+				case 6:
+
+				break;
+				case 7:
+
+				break;
+				case 8:
+
+				break;
+				case 9:
+
+				break;
+				//wyjscie z menu serwisowego !!!!!
+				case KEYCANCEL:
+					return 0;
+				break;
+				default:
+				break;
+			}
+
+
+
 		}
 
 	}
